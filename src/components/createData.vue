67a2126f7bf79d3808a6a38cbb4504693abf3017
sefuentespi <sefuentespi@unal.edu.co>
2020-12-23 11:05:47 -0500

    reto 4

--- src/components/createData.vue
+++ src/components/createData.vue
@@ -10,6 +10,19 @@
                     <input type="text" class="form-control" id="name" placeholder="Client Name" v-model="form.name">
                 </div>
                 <div class="form-group">
+                    <label for="formControlRange">Cliente Activo</label><br />
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="client_isActive" id="client_isActive" value="true"
+                            v-model="form.client_isActive">
+                        <label class="form-check-label" for="client_isActive">true</label>
+                    </div>
+                    <div class="form-check form-check-inline">
+                        <input class="form-check-input" type="radio" name="client_isActive" id="client_isActive" value="false"
+                            v-model="form.client_isActive">
+                        <label class="form-check-label" for="client_isActive">false</label>
+                    </div>
+                </div>
+                <div class="form-group">
                         <button class="btn btn-primary">Submit</button>
                 </div>
 
@@ -26,12 +39,13 @@
     import axios from 'axios';
 
 export default {
-    name: 'PutFormAxios',
+    name: 'PostFormAxios',
     data(){
         return{
             form: {
                 id: '',
                 name: '',
+                client_isActive: '',           
             },
             id: 0,
             name: "",
